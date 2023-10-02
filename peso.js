cebdfb4ec4101fec006dfb9d64c23391d93c6989
14102006h <113352801+14102006h@users.noreply.github.com>
2023-10-02 12:39:13 -0300

    Add files via upload
    02/10

--- peso.js
+++ peso.js
@@ -1,30 +1,18 @@
 //Cria referência ao form e elementos de reposta do programa
-
 const form = document.querySelector("form")
-const resp1 = document.querySelector("h3")
-const resp2 = document.querySelector("h4")
+const resp = document.querySelector("h3")
 
-//cria um ouvinte
-
+//ouvinte
 form.addEventListe("submit",(e) =>{
-e.preventDefault()              //evita o envio do form
+e.preventDefault()             
 
-const nome = form.inNome.Value           //obtém o nome
-const Sexo = Number(form.in.value) //obtém a primeira nota
-const Altura = Number(form.inAltura.value) //obtém a segunda nota
+const nome = form.InNome.value
+const masculino = form.InMasculino.checked
+const altura = Number(form.inAltura.value)
 
-const media = (nota1 + nota2)/2          //média das notas
-resp1.innerText = `Média das Notas ${media.tofixed(2)}`
-
-//Cria as condicionais
-if(media >= 7){
-    resp2.innerText= `Parabéns ${nome}! Você foi aprovado(a)`
-    resp2.style.color= "blue"
-}else if(media >=4){
-    resp2.innerText= `Atenção ${nome}! Você está de exame`
-    resp2.style.color= "green"
-}else{
-    resp2.innerText= `Ops ${nome}! Você foi reprovado(a)`
-    resp2.style.color= "red"
-}
+//calculando o peso ideal
+let peso
+ if (masculino) {
+    peso = 22 * Math.pow
+ }
 })

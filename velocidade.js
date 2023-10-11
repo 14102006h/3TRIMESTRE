0f156022af5cb722b16c013242cd768332cd20d5
14102006h <113352801+14102006h@users.noreply.github.com>
2023-10-11 12:39:49 -0300

    Add files via upload
    11/10

---
+++ velocidade.js
@@ -0,0 +1,29 @@
+alert("lknlknlkn")
+//Cria referência ao form e elementos de reposta do programa
+const form = document.querySelector("form")
+const resp = document.querySelector("h3")
+
+//ouvinte
+form.addEventListe("submit",(e) =>{
+e.preventDefault()             
+
+const nome = form.inNome.value
+const masculino = form.inMasculino.checked
+const altura = Number(form.inAltura.value)
+alert("lknlknlkn")
+
+//calculando o peso ideal
+let peso
+ if (masculino) {
+    peso = 22 * Math.pow(altura, 2)
+ } else {
+    peso = 21 * Math.pow(altura, 2)
+ }
+ //apreenta a resposta
+ resp.innerText = `${nome} seu peso ideal é: ${peso.toFixed(3)}kg`
+})
+//segundo ouvinte. limpa o conteudo de h3
+
+form.addEventListener("reset", (e) => {
+   resp.innerText = " "     //limpa o conteudo de h3
+})

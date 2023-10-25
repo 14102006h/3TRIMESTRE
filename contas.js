c25fd94fa7f71fe0b6b8d2c062c48d3f369025cb
14102006h <113352801+14102006h@users.noreply.github.com>
2023-10-25 12:23:01 -0300

    Add files via upload
    25/10

---
+++ contas.js
@@ -0,0 +1,17 @@
+const form = document.querySelector("form")
+const resp = document.querySelector("pre")
+
+//ouvinte
+form.addEventListener("submit",(e) => {
+    e.preventDefault()
+
+    const numero = Number(form.inNumero.value)
+    let resposta = ""   //variavel do tipo String, para concatenar a resposta
+    //cria um laço de repetição
+    for(let i = 1; i <=10; i++){
+        resposta= resposta + numero+ " x " + i +" = " + (numero * i) + "\n"
+     }
+     resp.innerText = resposta
+
+
+    })
